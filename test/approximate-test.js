633cb7fa786f5ee4d80c2576e6f0172ac5043240
Rustam Kasad <warriorpostman@gmail.com>
2015-01-13 23:08:57 -0800

    Parse more complex 'value'. Validate year kinda. Add more tests.

--- test/approximate-test.js
+++ test/approximate-test.js
@@ -7,6 +7,11 @@ describe('approximate parser ', function() {
     expect(approx).to.not.be.null;
     expect(approx.contextYear).to.equal(2015);
   });
+  
+  it('rejects bogus three-digit year', function(){
+    var func = function() { var approx = approxModule(491) };
+    expect(func).to.throw(Error);
+  });
 
   it('rejects missing MMM', function(){
     var approx = approxModule(2015)
@@ -28,13 +33,19 @@ describe('approximate parser ', function() {
 
   it('rejects bad (excess) DD', function(){
     var approx = approxModule(2015)
-    var func = function() { approx.parse('JAN 150') };
+    var func = function() { approx.parse('JAN 1500') };
     expect(func).to.throw(Error);
   });
 
   it('parses a simple colloquial date', function(){
     var approx = approxModule(2015)
-    var parsed = approx.parse('JAN 15');
+    var parsed = approx.parse('JAN 15 - Some Event - Some Bands');
+    expect(parsed.getTime()).to.equal(new Date('January 15, 2015').getTime());
+  });
+  
+  it('parses a simple colloquial date with subsequent', function(){
+    var approx = approxModule(2015)
+    var parsed = approx.parse('JAN 15 - Some Event - Some Bands');
     expect(parsed.getTime()).to.equal(new Date('January 15, 2015').getTime());
   });
 });
