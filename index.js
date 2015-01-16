f0d5fa0b89568f2fbca8ae1a7aff76da2ba29de9
Rustam Kasad <warriorpostman@gmail.com>
2015-01-15 21:26:12 -0800

    Wrap in module pattern.

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
-var proxy;
+(function() {
+  var proxy;
 
-module.exports = function( year ) {
-  if (!year.toString().match(/[0-9]{4}/g).length == 1)
-    throw new Error(year + ' is not a legit year');
-  var contextYear = year;
-  // TODO: do a check here that it's a legit number at least, and year 
-  proxy = { 
-    contextYear : contextYear, 
-    parse : function( value ) {
-      var lower = value.toLowerCase();
-      var matches = lower.match(
-          /(jan|feb|mar|apr|jun|jul|aug|sep|oct|nov|dec)\ +[0-9]{1,2}[^0-9]*$/g
-          );
-      if (matches && matches.length == 1) {
-        // so janky
-        var match = value.toLowerCase().match(
-         /(jan|feb|mar|apr|jun|jul|aug|sep|oct|nov|dec)\ +[0-9]{1,2}[^0-9]/g
-         );
-        return new Date(match + ', ' + contextYear); 
-      } else {
-        throw new Error(value + ' can\'t be parsed.');
+  module.exports = function( year ) {
+    if (!year.toString().match(/[0-9]{4}/g).length == 1)
+      throw new Error(year + ' is not a legit year');
+    var contextYear = year;
+    // TODO: do a check here that it's a legit number at least, and year 
+    proxy = { 
+      contextYear : contextYear, 
+      parse : function( value ) {
+        var lower = value.toLowerCase();
+        var matches = lower.match(
+            /(jan|feb|mar|apr|jun|jul|aug|sep|oct|nov|dec)\ +[0-9]{1,2}[^0-9]*$/g
+            );
+        if (matches && matches.length == 1) {
+          // so janky
+          var match = value.toLowerCase().match(
+           /(jan|feb|mar|apr|jun|jul|aug|sep|oct|nov|dec)\ +[0-9]{1,2}[^0-9]/g
+           );
+          return new Date(match + ', ' + contextYear); 
+        } else {
+          throw new Error(value + ' can\'t be parsed.');
+        }
       }
-    }
-  };
-  return proxy;
-}
+    };
+    return proxy;
+  }
+})()
 
