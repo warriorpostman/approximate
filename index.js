3bbf2511e245a6e42cf04d2051fc2e94d5c168cf
Rustam Kasad <warriorpostman@gmail.com>
2015-01-15 22:28:08 -0800

    Add parsing general date (eg 1/15, 12/4) Clean up the functions.

--- index.js
+++ index.js
@@ -5,24 +5,35 @@
     if (!year.toString().match(/[0-9]{4}/g).length == 1)
       throw new Error(year + ' is not a legit year');
     var contextYear = year;
-    // TODO: do a check here that it's a legit number at least, and year 
+
     proxy = { 
       contextYear : contextYear, 
+
       parse : function( value ) {
         var lower = value.toLowerCase();
         var matches = lower.match(
-            /(jan|feb|mar|apr|jun|jul|aug|sep|oct|nov|dec)\ +[0-9]{1,2}[^0-9]*$/g
+            /(jan|feb|mar|apr|jun|jul|aug|sep|oct|nov|dec)\ +[0-9]{1,2}[^0-9]/g
             );
         if (matches && matches.length == 1) {
-          // so janky
-          var match = value.toLowerCase().match(
-           /(jan|feb|mar|apr|jun|jul|aug|sep|oct|nov|dec)\ +[0-9]{1,2}[^0-9]/g
-           );
-          return new Date(match + ', ' + contextYear); 
+          return new Date(matches[0] + ', ' + contextYear); 
+        } else {
+          return proxy.parseGeneralDate( value )
+        }
+      },
+
+      parseGeneralDate : function( value ) {
+        var lower = value.toLowerCase();
+        var matches = lower.match(
+            /(1|2|3|4|5|6|7|8|9|10|11|12)\/[0-9]{1,2}[^0-9]/g
+            );
+        if (matches && matches.length == 1) {
+          return new Date(matches[0] + '/' + contextYear); 
         } else {
           throw new Error(value + ' can\'t be parsed.');
         }
       }
+
+      
     };
     return proxy;
   }
--- test/approximate-test.js
+++ test/approximate-test.js
@@ -3,11 +3,22 @@ var approxModule = require('../index');
 
 describe('approximate parser ', function() {
   it('caches context year', function(){
+
     var approx = approxModule(2015)
     expect(approx).to.not.be.null;
     expect(approx.contextYear).to.equal(2015);
   });
   
+  it('rejects undefined', function(){
+    var func = function() { var approx = approxModule(undefined) };
+    expect(func).to.throw(Error);
+  });
+
+  it('rejects null', function(){
+    var func = function() { var approx = approxModule(null) };
+    expect(func).to.throw(Error);
+  });
+
   it('rejects bogus three-digit year', function(){
     var func = function() { var approx = approxModule(491) };
     expect(func).to.throw(Error);
@@ -37,16 +48,46 @@ describe('approximate parser ', function() {
     expect(func).to.throw(Error);
   });
 
-  it('parses a simple colloquial date', function(){
+  it('parses a string containing multiple date MMM DD and returns the last match', function(){
+    var approx = approxModule(2015)
+    var func = function() { approx.parse('JAN 15 and FEB 23 - some bands playing') };
+    expect(func).to.throw(Error);
+  });
+  
+  it('parses a string containing simple date MMM DD', function(){
     var approx = approxModule(2015)
     var parsed = approx.parse('JAN 15 - Some Event - Some Bands');
     expect(parsed.getTime()).to.equal(new Date('January 15, 2015').getTime());
   });
   
-  it('parses a simple colloquial date with subsequent', function(){
+  it('parses a string containing date (MMM DD) with trailing text', function(){
     var approx = approxModule(2015)
     var parsed = approx.parse('JAN 15 - Some Event - Some Bands');
     expect(parsed.getTime()).to.equal(new Date('January 15, 2015').getTime());
+  });
+
+  it('parses a string containing date (MMM DD) with preceding text', function(){
+    var approx = approxModule(2015)
+    var parsed = approx.parse('Wednesday JAN 16 - Some Event - Some Bands');
+    expect(parsed.getTime()).to.equal(new Date('January 16, 2015').getTime());
+  });
+
+  it('parses a string containing date (MMM DD) with preceding text', function(){
+    var approx = approxModule(2015)
+    var parsed = approx.parse('Wednesday JAN 16 - Some Event - Some Bands');
+    expect(parsed.getTime()).to.equal(new Date('January 16, 2015').getTime());
+  });
+
+  it('parses a string containing date (MM/DD) with single-digit month', function(){
+    var approx = approxModule(2015)
+    var parsed = approx.parse('1/16 - Some Event - Some Bands');
+    expect(parsed.getTime()).to.equal(new Date('January 16, 2015').getTime());
+  });
+
+  it('parses a string containing date (MM/DD) with double-digit month', function(){
+    var approx = approxModule(2015)
+    var parsed = approx.parse('12/16 - Some Event - Some Bands');
+    expect(parsed.getTime()).to.equal(new Date('December 16, 2015').getTime());
   });
 });
 
